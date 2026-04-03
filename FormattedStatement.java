349c263ab1cf80cfe132253ee8c9c3610a14dd5d
clet001 <nwaforclement7@gmail.com>
2026-04-03 10:38:35 +0100

    assignment

---
+++ FormattedStatement.java
@@ -0,0 +1,13 @@
+public class FormattedStatement {
+
+    public static void main(String[] args) {
+
+        System.out.printf("Nigeria became independent in %d and is full of great minds. I am one of them. My name is %s and I weigh %d%s and I am a %C", 1960,"CEO", 72, "kg", 'M');
+
+
+            int numb1 = 20
+            int numb2 = 30w
+
+    }
+
+}
