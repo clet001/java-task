d945d717ed5bffbb3b09ddb2514adfedfb117603
clet001 <nwaforclement7@gmail.com>
2026-04-08 04:50:55 +0100

    BitterKola

---
+++ EasterEggs/BitterKola/Speed.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+    public class Speed {
+
+    public static void main(String[] args){
+    Scanner inputCollector = new Scanner (System.in);
+
+    System.out.println("What is the speed?");
+        int speed = inputCollector.nextInt();
+
+
+
+int Speed = 1;
+
+
+   if (Speed <= 1){
+        System.out.println("Stationary");
+    }     
+
+    else{ 
+    if (speed >= 40){
+    System.out.println("Slow");
+    }
+
+        if (Speed >= 80){
+        System.out.println("Moderate");
+    }     
+
+    else{ 
+    if (speed <= 120){
+    System.out.println("Dangerously Fast");
+    }
+
+}
+}
+
+
+
+
+    }
+
+
+
+}
