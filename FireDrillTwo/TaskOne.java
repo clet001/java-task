a97cb326e770f3f1395ceb6c1a163ccd27d5d5fc
clet001 <nwaforclement7@gmail.com>
2026-04-15 17:06:05 +0100

    TaskOne

---
+++ FireDrillTwo/TaskOne.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class TaskOne{
+    public static void main(String[] args){
+
+
+for(int count = 1; count <= 10; count++){
+
+
+
+if(count + count == 10){
+
+System.out.print(count);
+}
+
+
+}
+
+}
+
+}
